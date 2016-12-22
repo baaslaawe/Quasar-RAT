5007da805f6e85ccb04a2fe1794a90036a938ec2
Mirko Bastianini <mirko.bastianini@gmail.com>
2016-12-22 14:07:53 +0100

    Add files via upload

---
+++ Client/Core/NAudio/Utils/WavePositionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using xClient.Core.NAudio.Wave.WaveOutputs;
+
+namespace xClient.Core.NAudio.Utils
+{
+    /// <summary>
+    /// WavePosition extension methods
+    /// </summary>
+    public static class WavePositionExtensions
+    {
+        /// <summary>
+        /// Get Position as timespan
+        /// </summary>
+        public static TimeSpan GetPositionTimeSpan(this IWavePosition @this)
+        {
+            var pos = @this.GetPosition() / (@this.OutputWaveFormat.Channels * @this.OutputWaveFormat.BitsPerSample / 8);
+            return TimeSpan.FromMilliseconds(pos * 1000.0 / @this.OutputWaveFormat.SampleRate);
+        }
+    }
+}
---
+++ Client/Core/NAudio/Wave/WaveOutputs/PlaybackState.cs
@@ -0,0 +1,21 @@
+﻿namespace xClient.Core.NAudio.Wave.WaveOutputs
+{
+    /// <summary>
+    /// Playback State
+    /// </summary>
+    public enum PlaybackState
+    {
+        /// <summary>
+        /// Stopped
+        /// </summary>
+        Stopped,
+        /// <summary>
+        /// Playing
+        /// </summary>
+        Playing,
+        /// <summary>
+        /// Paused
+        /// </summary>
+        Paused
+    }
+}
