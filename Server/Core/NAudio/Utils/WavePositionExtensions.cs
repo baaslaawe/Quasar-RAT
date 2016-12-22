bfe5e3d1025860a63285d5ec12f8541bedbeef2e
Mirko Bastianini <mirko.bastianini@gmail.com>
2016-12-22 14:19:38 +0100

    Add files via upload

---
+++ Server/Core/NAudio/Utils/WavePositionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using xServer.Core.NAudio.Wave.WaveOutputs;
+
+namespace xServer.Core.NAudio.Utils
+{
+    /// <summary>
+    /// WavePosition extension methods
+    /// </summary>
+    public static class WavePositionExtensions
+    {
+        /// <summary>
+        /// Get Position as timespan
+        /// </summary>
+        public static TimeSpan GetPositionTimeSpan(this IWavePosition @this)
+        {
+            var pos = @this.GetPosition() / (@this.OutputWaveFormat.Channels * @this.OutputWaveFormat.BitsPerSample / 8);
+            return TimeSpan.FromMilliseconds(pos * 1000.0 / @this.OutputWaveFormat.SampleRate);
+        }
+    }
+}
