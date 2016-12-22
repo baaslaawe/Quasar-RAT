78bf613d0d1b7ebea723f7bef8f65ed9b0488bbe
Mirko Bastianini <mirko.bastianini@gmail.com>
2016-12-22 14:05:33 +0100

    Add files via upload

---
+++ Client/Core/AForge/Video.DirectShow/Internals/ISpecifyPropertyPages.cs
@@ -0,0 +1,35 @@
+// AForge Direct Show Library
+// AForge.NET framework
+//
+// Copyright © Andrew Kirillov, 2008
+// andrew.kirillov@gmail.com
+//
+
+using System.Runtime.InteropServices;
+
+namespace xClient.Core.AForge.Video.DirectShow.Internals
+{
+    /// <summary>
+    /// The interface indicates that an object supports property pages.
+    /// </summary>
+    /// 
+    [ComImport,
+    Guid( "B196B28B-BAB4-101A-B69C-00AA00341D07" ),
+    InterfaceType( ComInterfaceType.InterfaceIsIUnknown )]
+    internal interface ISpecifyPropertyPages
+    {
+        /// <summary>
+        /// Fills a counted array of GUID values where each GUID specifies the
+        /// CLSID of each property page that can be displayed in the property
+        /// sheet for this object.
+        /// </summary>
+        /// 
+        /// <param name="pPages">Pointer to a CAUUID structure that must be initialized
+        /// and filled before returning.</param>
+        /// 
+        /// <returns>Return's <b>HRESULT</b> error code.</returns>
+        /// 
+        [PreserveSig]
+        int GetPages( out CAUUID pPages );
+    }
+}
